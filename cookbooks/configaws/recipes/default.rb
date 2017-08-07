ea36dd70496042f7ff6b44913d441cd917d9b517
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-08-07 16:26:24 -0500

    add cloudcli_aws_credentials

--- cookbooks/configaws/recipes/default.rb
+++ cookbooks/configaws/recipes/default.rb
@@ -3,20 +3,31 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe "cloudcli"
+
 package 'git'
 package 'docker'
 
 service 'docker' do
 	action :start
 end
-if node['platform_family'] == "debian" 
+if node['platform_family'] == "debian"
 	package 'python-pip'
 end
 
 if node['platform'] == "redhat"
 	package 'python2-pip'
-	
+
 	execute 'awscli' do
 		command 'pip install awscli'
 	end
+end
+
+cloudcli_aws_credentials '/etc/aws/credentials' do
+  owner 'cloud_user'
+  group 'cloud_user'
+  mode 0600
+  credential_params(
+    region: 'us-west-2'
+  )
 end
