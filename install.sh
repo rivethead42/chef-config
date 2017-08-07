e5a61443c1f71e9b94b739e208ce05091eb211a2
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-08-07 16:15:14 -0500

    add cloudcli to run_list

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd
+wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
+rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
 cp aws.json solo.rb /
 cd /
 chef-solo -c solo.rb -j aws.json
