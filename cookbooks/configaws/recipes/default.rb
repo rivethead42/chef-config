29b31dfe3c7e6956cfb55fb19bbcc998f3047858
root <root@tthomsen5.mylabserver.com>
2017-08-08 18:35:01 +0000

    add ssh, add cloud_user

--- cookbooks/configaws/recipes/default.rb
+++ cookbooks/configaws/recipes/default.rb
@@ -3,31 +3,50 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe "cloudcli_aws_credentials"
+
+include_recipe 'sshd::default'
 
 package 'git'
 package 'docker'
+
+user 'cloud_user' do
+  home '/home/cloud_user'
+  manage_home true
+  shell '/bin/bash'
+  password '$1$linuxaca$iGMxZ4g4lbPmfEDPhW3lw1'
+  salt 'linuxacademy'
+end
+
+openssh_server '/etc/sshd_config' do
+  PasswordAuthentication yes
+end
 
 service 'docker' do
 	action :start
 end
 if node['platform_family'] == "debian"
-	package 'python-pip'
+  package 'python-pip'
+  package 'awscli'
 end
 
-if node['platform'] == "redhat"
-	package 'python2-pip'
+if node['platform_family'] == "rhel"
+  package 'python2-pip'
+  	
+  execute 'curl-get-pip' do
+    command 'curl -O https://bootstrap.pypa.io/get-pip.py'
+  end
+	
+  execute 'get-pip' do
+    command 'python get-pip.py --user'
+  end
 
-	execute 'awscli' do
-		command 'pip install awscli'
-	end
+  execute 'awscli' do
+    command 'pip install awscli --upgrade --user'
+  end
+
+  #ruby_block "update bash_profile" do
+  #	fe = Chef::Util::FileEdit.new("/root/.bash_profile")
+  #	fe.insert_line_if_no_match(/PATH=$PATH:$HOME/bin/, "PATH=$PATH:$HOME/bin:~/.local/bin:$PATH")
+  #end
 end
 
-cloudcli_aws_credentials '/etc/aws/credentials' do
-  owner 'cloud_user'
-  group 'cloud_user'
-  mode 0600
-  credential_params(
-    region: 'us-west-2'
-  )
-end
