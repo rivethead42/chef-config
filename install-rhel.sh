f3380f7ad032adc6e65ff124120f208861953e84
root <root@tthomsen5.mylabserver.com>
2017-08-08 19:40:45 +0000

    add install scripts

---
+++ install-rhel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd 
+wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
+rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
---
+++ install-ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd 
+wget https://packages.chef.io/files/stable/chefdk/2.0.28/ubuntu/16.04/chefdk_2.0.28-1_amd64.deb
+dpkg -i chefdk_2.0.28-1_amd64.deb
