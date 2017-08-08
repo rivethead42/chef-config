478fdfdc8545b99de0c9871a8fec7af02492e2a4
root <root@tthomsen5.mylabserver.com>
2017-08-08 20:17:30 +0000

    add epel to install script

--- install-rhel.sh
+++ install-rhel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd 
+yum install epel-release -y 
+yum update -y
 wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
 rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
