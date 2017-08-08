86e19acf9c654ae34c65bc9174a9ddc8411a3f30
root <root@tthomsen5.mylabserver.com>
2017-08-08 19:55:38 +0000

    fix copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd
-wget https://packages.chef.io/files/stable/chefdk/2.0.28/el/7/chefdk-2.0.28-1.el7.x86_64.rpm
-rpm -ivh chefdk-2.0.28-1.el7.x86_64.rpm
-cp aws.json solo.rb /
+cp ~/chef-config/aws.json ~/chef-config/solo.rb /
 cd /
 chef-solo -c solo.rb -j aws.json
