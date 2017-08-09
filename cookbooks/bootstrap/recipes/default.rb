8091360e951ffc5c8ff97804398429d48b036c11
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-08-09 11:20:11 -0500

    add tests

--- cookbooks/bootstrap/recipes/default.rb
+++ cookbooks/bootstrap/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook:: test
+# Cookbook:: bootstrap
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
@@ -7,12 +7,10 @@
 include_recipe 'sshd::default'
 include_recipe 'cloudcli::default'
 
-execute 'echo "stop sshd" >> /root/chef.log' do end
 service 'sshd' do
   action :stop
 end
 
-execute 'echo "install git" >> /root/chef.log' do end
 package 'git'
 
 if node['platform_family'] == "debian"
@@ -34,54 +32,28 @@ if node['platform_family'] == "debian"
 end
 
 if node['platform_family'] == "rhel"
-  execute 'echo "install docker" >> /root/chef.log' do end
   package 'docker'
 
-  execute 'echo "start  docker service" >> /root/chef.log' do end
   service 'docker' do
     action :start
   end
 
   execute 'echo "python2-pip" >> /root/chef.log' do end
   package 'python2-pip'
-
-  #execute 'echo "get pip" >> /root/chef.log' do end
-  #execute 'curl-get-pip' do
-  #  command 'curl -O https://bootstrap.pypa.io/get-pip.py'
-  #end
-
-  #execute 'echo "install get-pip" >> /root/chef.log' do end
-  #execute 'get-pip' do
-  #  command 'python get-pip.py --user'
-  #end
-
-  #execute 'echo "pip install awscli" >> /root/chef.log' do end
-  #execute 'awscli' do
-  #  command 'pip install awscli --upgrade --user'
-  #end
-
-  #execute 'echo "update path in .bash_profile" >> /root/chef.log' do end
-  #execute 'Update Path' do
-  #  command 'echo "PATH=~/.local/bin:\$PATH" >> /root/.bash_profile && echo "export PATH" >>  /root/.bash_profile'
-  #end
 end
 
-execute 'echo "install photo-filter" >> /root/chef.log' do end
 execute 'install photo-filter' do
   command 'docker pull cloudassessmentscom/photo-filter'
 end
 
-execute 'echo "install photo-storage" >> /root/chef.log' do end
 execute 'install photo-storage' do
   command 'docker pull cloudassessmentscom/photo-storage'
 end
 
-execute 'echo "install web-client" >> /root/chef.log' do end
 execute 'install web-client' do
   command 'docker pull cloudassessmentscom/web-client'
 end
 
-execute 'echo "start sshd" >> /root/chef.log' do end
 service 'sshd' do
   action :start
 end
--- cookbooks/bootstrap/test/smoke/default/default_test.rb
+++ cookbooks/bootstrap/test/smoke/default/default_test.rb
@@ -7,7 +7,7 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
- describe user('cloud_user'), :skip do
+  describe user('cloud_user'), :skip do
     it { should exist }
   end
 end
@@ -20,41 +20,53 @@ end
 #
 if os[:family] == 'debian'
   describe package('python-pip') do
-   it { should be_installed}
+    it { should be_installed}
   end
 
   describe package('awscli') do
-   it { should be_installed}
+    it { should be_installed}
   end
 end
 
 if os[:family] == 'redhat'
 
- describe package('git') do
-  it { should be_installed }
- end
+  describe package('git') do
+    it { should be_installed }
+  end
 
- describe package('python-pip') do
-  it { should be_installed}
- end
+  describe package('python-pip') do
+    it { should be_installed}
+  end
 
- describe package('docker') do
-  it { should be_installed }
- end
+  describe package('docker') do
+    it { should be_installed }
+  end
 
- describe service('docker') do
-  it { should be_running }
- end
+  describe service('docker') do
+    it { should be_running }
+  end
 
- describe docker_image('cloudassessmentscom/photo-filter:latest') do
-  it { should exist }
- end
+  describe file('/usr/bin/aws') do
+    it { should exist }
+  end
 
- describe docker_image('cloudassessmentscom/photo-storage:latest') do
-  it { should exist }
- end
+  describe file('/etc/ssh/sshd_config') do
+    its(:content) { should contain 'PasswordAuthentication yes' }
+  end
 
- describe docker_image('cloudassessmentscom/web-client:latest') do
-  it { should exist }
- end
+  describe docker_image('cloudassessmentscom/photo-filter:latest') do
+    it { should exist }
+  end
+
+  describe docker_image('cloudassessmentscom/photo-storage:latest') do
+    it { should exist }
+  end
+
+  describe docker_image('cloudassessmentscom/web-client:latest') do
+    it { should exist }
+  end
+
+  describe service('sshd') do
+    it { should be_running }
+  end
 end
